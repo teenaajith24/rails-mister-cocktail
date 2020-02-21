4688cd727708d7be197df63dcb910c6008b3050f
teena ajith <teena.ajith@gmail.com>
2020-02-21 17:15:42 +0100

    made some changes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -17,9 +17,9 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new(cocktail_params)
 
     if @cocktail.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktails_path(@cocktail)
     else
-      render 'new'
+      render :'new'
     end
   end
 
